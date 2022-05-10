23671862b8f60d4c5da53c7884c69fdbc4106aae
Eric Hurst <ericahurst77@gmail.com>
2022-05-09 23:59:33 -0400

    updated api category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,16 +5,27 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll({ include: [Product],})
   // be sure to include its associated Products
+  .then((categories) => res.json(categories))
+  .catch((err) => res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  Category.findOne ({  where: { id: req.params.id,},
   // be sure to include its associated Products
+   include: [Product],
+  })
+  .then((category) => res.json(category))
+  .catch((err) => res.status(500).json(err));
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+  .then((category) => res.status(200).json(category))
+  .catch((err) => res.status(400).json(err));
 });
 
 router.put('/:id', (req, res) => {
